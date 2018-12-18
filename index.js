26f8aa50af944893cb47cc5bb9a1e5b3fbe26dd3
chrismacdonaldw <chrmacdonal5@upei.ca>
2018-12-18 17:13:02 -0400

    full sorting and lastname

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const colors = [
 const lastName = 'MacDonald';
 
 var timeline = document.getElementById('timeline');
+var lastdiv = document.getElementById('lastName');
 var size = prov.length;
 
 for (var i = 0; i < size; i++) {
@@ -43,63 +44,92 @@ for (var i = 0; i < size; i++) {
     $(selector).hide();
 }
 
+// Create last name div
+for(var i = 0; i < lastName.length; i++) {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var index = lastName.charAt(i);
+    var spanned = document.createElement('span');
+    spanned.style.color = color;
+    spanned.classList.add('lastLetter');
+    var last = document.createTextNode(index);
+    spanned.appendChild(last);
+    lastdiv.append(spanned);
+}
+
 // Fade in, when complete, sort
 $('.square').fadeIn('slow', function() {
     var sortTimeline = anime.timeline();
 
     sortTimeline.add({
         targets: '#timeline .square.o1',
+        duration: 500,
         translateY: 100
     })
     .add({
         targets: '#timeline .square.o0',
+        duration: 500,
         translateY: 220
     })
     .add({
         targets: '#timeline .square.o1',
+        duration: 500,
         translateX: [ { value: 200 }, { value: 200 } ],
         translateY: [ { value: 100 }, { value: 0 }],
     })
     .add({
         targets: '#timeline .square.o0',
+        duration: 500,
         translateX: [ { value: -200 }, { value: -200 } ],
         translateY: [ { value: 220 }, { value: 0 } ]
     })
     .add({
         targets: '#timeline .square.o2',
+        duration: 500,
         translateY: 100
     })
     .add({
         targets: '#timeline .square.o1',
+        duration: 500,
         translateY: 220,
         translateX: 200
     })
     .add({
         targets: '#timeline .square.o2',
+        duration: 500,
         translateX: [ { value: 100 }, { value: 100 } ],
         translateY: [ { value: 100 }, { value: 0 }]
     })
     .add({
         targets: '#timeline .square.o1',
+        duration: 500,
         translateX: [ { value: 100 }, { value: 100 } ],
         translateY: [ { value: 220 }, { value: 0 } ]
     })
     .add({
         targets: '#timeline .square.o4',
+        duration: 500,
         translateY: 100
     })
     .add({
         targets: '#timeline .square.o3',
+        duration: 500,
         translateY: 220
     })
     .add({
         targets: '#timeline .square.o4',
+        duration: 500,
         translateX: [ { value: 100 }, { value: 100 } ],
         translateY: [ { value: 100 }, { value: 0 } ]
     })
     .add({
         targets: '#timeline .square.o3',
+        duration: 500,
         translateX: [ { value: -100 }, { value: -100 } ],
         translateY: [ { value: 220 }, { value: 0 } ]
+    })
+    .add({
+        targets: '.lastLetter',
+        opacity: 1,
+        duration: 2000
     })
 });
